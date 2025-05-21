6c5a27cb593290c38cc2c3bd2241c91e66130d05
Aeonics <aeonics@live.no>
2025-05-20 23:06:13 -0600

    Ejercicio de dados con diferentes salidas

---
+++ Parte 2/Dice_2/Program.cs
@@ -0,0 +1,8 @@
+﻿Random dice = new Random();
+int roll1 = dice.Next(); //Genera un número aleatorio entre 0 y Int32.MaxValue
+int roll2 = dice.Next(101); //Genera un número aleatorio entre 0 y 100
+int roll3 = dice.Next(30, 101); //Genera un número aleatorio entre 30 y 100
+
+Console.WriteLine($"Tirando el dado... {roll1}");
+Console.WriteLine($"Tirando el dado... {roll2}");
+Console.WriteLine($"Tirando el dado... {roll3}");

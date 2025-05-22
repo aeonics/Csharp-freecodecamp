bfe3b1a9ff78cf17258a032c3bac8bc53deff384
Aeonics <aeonics@live.no>
2025-05-21 23:05:15 -0600

    Ejercicios If Else

---
+++ Parte 2/IF_Else/Program.cs
@@ -0,0 +1,29 @@
+﻿Random dado = new Random();
+
+int roll1 = dado.Next(1, 7); //Genera un número aleatorio entre 1 y 6
+int roll2 = dado.Next(1, 7); //Genera un número aleatorio entre 1 y 6
+int roll3 = dado.Next(1, 7); //Genera un número aleatorio entre 1 y 6
+
+int suma = roll1 + roll2 + roll3;
+Console.WriteLine($"Dado roll... {roll1} + {roll2} + {roll3} = {suma}");
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    Console.WriteLine("Tiraste dobles! +2 puntos");
+    suma += 2;
+}
+if ((roll1 == roll2) && (roll2 == roll3)) 
+{
+    Console.WriteLine("You rolled triples! +6 bonus to total!");
+    suma += 6;
+}
+
+if (suma >= 15)
+{
+    Console.WriteLine("Ganaste");
+}
+if (suma < 15)
+{
+    Console.WriteLine("Perdiste");
+}
+
+
